f9e1a132213b3eaeb261c2980e9d249deb3e13b9
rmobis <r.mobis@gmail.com>
2015-10-10 17:15:49 -0300

    Replace jQuery Animate Number with Odometer Still trying to get rid of jQuery

--- build.js
+++ build.js
@@ -5,3 +5,5 @@ var fs   = require('fs-extra'),
 fs.writeFile('dist/index.html', swig.renderFile('index.swig'));
 fs.copy('assets/', 'dist/assets/', noop);
 fs.copy('bower_components/vivus/dist/vivus.min.js', 'dist/assets/js/vendor/vivus.min.js', noop);
+fs.copy('bower_components/odometer/themes/odometer-theme-minimal.css', 'dist/assets/css/vendor/odometer-theme-minimal.css', noop);
+fs.copy('bower_components/odometer/odometer.min.js', 'dist/assets/js/vendor/odometer.min.js', noop);
