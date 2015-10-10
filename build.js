22f37b01b88f1083b596f88e371616085a34b2e5
rmobis <r.mobis@gmail.com>
2015-10-10 16:16:53 -0300

    Separate sections into partials using Swig

---
+++ build.js
@@ -0,0 +1,7 @@
+var fs   = require('fs'),
+	ncp  = require('ncp'),
+	swig = require('swig');
+
+fs.writeFile('dist/index.html', swig.renderFile('index.html'));
+ncp('assets/', 'dist/assets/');
+ncp('bower_components/', 'dist/bower_components/');
