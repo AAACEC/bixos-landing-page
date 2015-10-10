89b9bfbcddd1f044723c4b4e72f37476697dbfc9
rmobis <r.mobis@gmail.com>
2015-10-10 16:35:53 -0300

    Replace jQuery Lazy Line Painter with Vivus Trying to get rid of jQuery

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
-var fs   = require('fs'),
-	ncp  = require('ncp'),
-	swig = require('swig');
+var fs   = require('fs-extra'),
+	swig = require('swig'),
+	noop = require('node-noop').noop;
 
 fs.writeFile('dist/index.html', swig.renderFile('index.swig'));
-ncp('assets/', 'dist/assets/');
-ncp('bower_components/', 'dist/bower_components/');
+fs.copy('assets/', 'dist/assets/', noop);
+fs.copy('bower_components/vivus/dist/vivus.min.js', 'dist/assets/js/vendor/vivus.min.js', noop);
