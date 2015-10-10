9791301aba5e6cb296da42e320020606f517a5a3
rmobis <r.mobis@gmail.com>
2015-10-10 16:28:52 -0300

    Fix template file name on build script

--- build.js
+++ build.js
@@ -2,6 +2,6 @@ var fs   = require('fs'),
 	ncp  = require('ncp'),
 	swig = require('swig');
 
-fs.writeFile('dist/index.html', swig.renderFile('index.html'));
+fs.writeFile('dist/index.html', swig.renderFile('index.swig'));
 ncp('assets/', 'dist/assets/');
 ncp('bower_components/', 'dist/bower_components/');
