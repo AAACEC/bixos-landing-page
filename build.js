94dbf8e651cbba2bfa12cfa79ec9be7b36d08a21
rmobis <r.mobis@gmail.com>
2015-10-11 02:08:03 -0300

    Replace jQuery Skrollr with ScrollMagic The quest for a jQuery-free project continues

--- build.js
+++ build.js
@@ -2,8 +2,16 @@ var fs   = require('fs-extra'),
 	swig = require('swig'),
 	noop = require('node-noop').noop;
 
+// TODO: Handle multiple build configs (release/debug)
 fs.writeFile('dist/index.html', swig.renderFile('index.swig'));
+
 fs.copy('assets/', 'dist/assets/', noop);
 fs.copy('bower_components/vivus/dist/vivus.min.js', 'dist/assets/js/vendor/vivus.min.js', noop);
+
 fs.copy('bower_components/odometer/themes/odometer-theme-minimal.css', 'dist/assets/css/vendor/odometer-theme-minimal.css', noop);
 fs.copy('bower_components/odometer/odometer.min.js', 'dist/assets/js/vendor/odometer.min.js', noop);
+
+fs.copy('bower_components/scrollmagic/scrollmagic/uncompressed/ScrollMagic.js', 'dist/assets/js/vendor/scrollmagic.js', noop);
+fs.copy('bower_components/scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js', 'dist/assets/js/vendor/scrollmagic.addIndicators.js', noop);
+fs.copy('bower_components/scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js', 'dist/assets/js/vendor/scrollmagic.gsap.js', noop);
+fs.copy('bower_components/gsap/src/uncompressed/TweenMax.js', 'dist/assets/js/vendor/TweenMax.js', noop);
